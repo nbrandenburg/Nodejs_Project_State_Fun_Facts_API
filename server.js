fc101ecd94691226e51232572b2b5d84a4668584
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-05-01 16:33:03 -0500

    testing cors issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
 // Connect to MongoDB
 connectDB();
