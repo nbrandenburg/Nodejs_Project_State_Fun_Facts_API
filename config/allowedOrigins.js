f59f570129c8ec037fda2286ffbd9d83c913b87d
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-05-01 16:06:45 -0500

    testing cors issue

--- config/allowedOrigins.js
+++
@@ -1,8 +0,0 @@
-const allowedOrigins = [
-    'https://dazzling-snickerdoodle-777101.netlify.app/',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
-];
-
-module.exports = allowedOrigins;
