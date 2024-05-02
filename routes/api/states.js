e350aac3e6390871a526a8e095b8b429d4d291b3
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-05-02 17:10:31 -0500

    testing

--- routes/api/states.js
+++ routes/api/states.js
@@ -5,29 +5,26 @@ const statesController = require('../../controllers/statesController');
 router.route('/')
     .get(statesController.getAllStates)
     
-    router.route('/states')
-        .get(statesController.getAllStates)
+router.route('/states')
+    .get(statesController.getAllStates)
 
-        router.route('/:state/capital')
-            .get(statesController.getStateCapital)
+router.route('/:state/capital')
+    .get(statesController.getStateCapital)
 
-        router.route('/:state/nickname')
-            .get(statesController.getStateNickname)
+router.route('/:state/nickname')
+    .get(statesController.getStateNickname)
 
-        router.route('/:state/population')
-            .get(statesController.getStatePopulation)
+router.route('/:state/population')
+    .get(statesController.getStatePopulation)
 
-        router.route('/:state/admission')
-            .get(statesController.getStateAdmission)
+router.route('/:state/admission')
+    .get(statesController.getStateAdmission)
 
-        router.route('/:state/funfact')
-            .get(statesController.getStateFunfact)
-            .post(statesController.createNewState)
-            .patch(statesController.updateState)
-            .delete(statesController.deleteState)
-
-
-
+router.route('/:state/funfact')
+    .get(statesController.getStateFunfact)
+    .post(statesController.createNewState)
+    .patch(statesController.updateState)
+    .delete(statesController.deleteState)
 
 router.route('/:state')
     .get(statesController.getState)     
