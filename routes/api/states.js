26d65010bb2ebe0da9bd79350730a1e7e330930c
Nicole Brandenburg <nicolebrandenburg@hotmail.com>
2024-05-02 16:34:51 -0500

    updated routes

--- routes/api/states.js
+++ routes/api/states.js
@@ -5,28 +5,28 @@ const statesController = require('../../controllers/statesController');
 router.route('/')
     .get(statesController.getAllStates)
     
-    router.route('/states')
+router.route('/states')
     .get(statesController.getAllStates)
 
-    router.route('/:state/capital')
+router.route('/states/:state/capital')
     .get(statesController.getStateCapital)
-    
-    router.route('/:state/nickname')
+
+router.route('/states/:state/nickname')
     .get(statesController.getStateNickname)
 
-    router.route('/:state/population')
+router.route('/states/:state/population')
     .get(statesController.getStatePopulation)
 
-    router.route('/:state/admission')
+router.route('/states/:state/admission')
     .get(statesController.getStateAdmission)
 
-    router.route('/:state/funfact')
+router.route('/states/:state/funfact')
     .get(statesController.getStateFunfact)
     .post(statesController.createNewState)
     .patch(statesController.updateState)
     .delete(statesController.deleteState)
 
-    router.route('/:state')
-        .get(statesController.getState)     
+router.route('/states/:state')
+    .get(statesController.getState)     
 
 module.exports = router;
